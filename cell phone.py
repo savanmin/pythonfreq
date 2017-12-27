3638cb59906b31976a5da5f5f3651268dc31c322
Savan Minsariya <savanminsariya18@gmail.com>
2017-12-27 19:26:59 +0530

    [Modified] cell phone programme modified

--- cell phone.py
+++ cell phone.py
@@ -1,25 +1,27 @@
-bill = False
 
 minute = float(input("Please enter total minutes you have used this month : "))
 text = float(input("Please enter total texts you have used this month : "))
 
-base = 15
-fee_911 = 0.44
+base = float(15)
+fee_911 = float(0.44)
 
-if minute >50:
-    base += (minute-50)* 0.25
-    print ('you exhausted your monthly plan and your additional charge for mintues are  : %0.2f ') % ((minute-50)* 0.25)
+basem = 0
+baset = 0
+
+if minute > 50:
+    basem += (minute-50)* 0.25
+    print ('you exhausted your monthly plan and your additional charge for mintues are  : %0.2f ' % (float(minute-50)* 0.25))
 if text  > 50:
-    base += (text-50) * 0.15
-    print ('you exhausted your monthly plan and your additional charge for texts are : %0.2f  ') % ((text-50) * 0.15)
+    baset += (text-50) * 0.15
+    print ('you exhausted your monthly plan and your additional charge for texts are : %0.2f  ' % (float(text-50) * 0.15))
 
-sub_total = base + fee_911
+sub_total = base + basem + baset + fee_911
 tax = sub_total * 0.05
 totalbill = sub_total + tax
 
 print ('Base                            : %0.2f' % base)
-print ('Additional minutes charges      : %0.2f'  % ((minute-50)* 0.25))
-print ('Additional text charges         : %0.2f'  % ((text-50) * 0.15))
+print ('Additional minutes charges      : %0.2f'  % (float(minute-50)* 0.25))
+print ('Additional text charges         : %0.2f'  % (float(text-50) * 0.15))
 print ('911 Fee                         : 0.44')
 print ('Sub Total                       : %0.2f' % sub_total)
 print ('Tax                             : %0.2f' % tax)
